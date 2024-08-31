34bde2ffc6e90b174ebd0678b69258fa861595d6
Yashavant Singh <yashavantsingh01.hp@outlook.com>
2024-08-31 11:12:22 +0530

    added some responsive styling to it

--- src/Component/Shared/FAQ/Faq.tsx
+++ src/Component/Shared/FAQ/Faq.tsx
@@ -45,7 +45,9 @@ const FAQ = () => {
   }, [state]);
   return (
     <div className="bg-black w-full h-auto" id="FAQ">
-      <div className="min-h-screen bg-black text-white max-w-screen-xl mx-auto px-4">
+       <div className="max-w-screen-xl mx-auto px-2">
+        {/* Styling added by Yash */}
+      <div className="border-2 text-white  border-dotted border-gray-700 p-4 max-w-7xl mx-auto rounded-lg">
         <div className="border-dotted border-2 border-gray-600 p-8 rounded-xl flex flex-col lg:flex-row items-center justify-between">
           <div className="text-left mb-6 lg:mb-0 mt-10 lg:mt-0">
             <h2 className="text-3xl font-bold mb-2">
@@ -102,6 +104,7 @@ const FAQ = () => {
             </div>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
